6a23c98fc2eb95791b6e376c8a0ecc0a8fe62902
idircarlos <idircarlos@gmail.com>
2026-08-28 20:44:26 +0200

    Bumping version to 0.1.0

--- include/metadata.h
+++ include/metadata.h
@@ -1,7 +1,7 @@
 #ifndef METADATA_H_
 #define METADATA_H_
 
-#define BO1ZT_VERSION "0.0.1a"
+#define BO1ZT_VERSION "0.1.0"
 #define BO1ZT_BY "rayito106 (aka idircarlos)"
 #define BO1ZT_GITHUB_URL "https://github.com/idircarlos/bo1zt"
 #define BO1ZT_DISCORD_URL "https://discord.gg/zcFkKheNfG"
